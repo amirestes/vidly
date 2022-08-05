2e68ed2677faa806495cd00945629afd86670a90
amirestes <amirestes@gmail.com>
2022-08-04 23:59:08 -0500

    wrong code

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -10,23 +10,23 @@ const Pagination = (props) => {
   if (pagesCount === 1) return null;
   const pages = _.range(1, pagesCount + 1);
 
-//   return (
-//     <nav>
-//       <ul className="pagination">
-//         {pages.map((page) => (
-//           <li
-//             key={page}
-//             className={page === currentPage ? "page-item active" : "page-item"}
-//           >
-//             <a className="page-link" onClick={() => onPageChange(page)}>
-//               {page}
-//             </a>
-//           </li>
-//         ))}
-//       </ul>
-//     </nav>
-//   );
-// };
+  return (
+    <nav>
+      <ul className="pagination">
+        {pages.map((page) => (
+          <li
+            key={page}
+            className={page === currentPage ? "page-item active" : "page-item"}
+          >
+            <a className="page-link" onClick={() => onPageChange(page)}>
+              {page}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
 Pagination.propTypes = {
   itemsCount: PropTypes.number.isRequired,
